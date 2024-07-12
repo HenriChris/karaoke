324d44324151910c3206020f1fef2ccd23536cde
Henrique <henrique@ejcm.com.br>
2024-07-12 19:02:29 -0300

    qr code

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,10 @@
-/* import React, { useState, useEffect } from 'react';
-import Papa from 'papaparse';
-import Pagination from './components/Pagination';
-import SearchBar from './components/SearchBar';
-import SongList from './components/SongList';
-
-interface Song {
-    name: string;
-    author: string;
-    code: string;
-}
-
-const App: React.FC = () => {
-    const [nationalSongs, setNationalSongs] = useState<Song[]>([]);
-    const [internationalSongs, setInternationalSongs] = useState<Song[]>([]);
-    const [songs, setSongs] = useState<Song[]>([]);
-    const [filteredSongs, setFilteredSongs] = useState<Song[]>([]);
-    const [searchQuery, setSearchQuery] = useState<string>('');
-    const [currentPage, setCurrentPage] = useState<number>(1);
-
-    const songsPerPage = 100;
-
-    const handleFileToggleInt = () => {
-        setSongs(internationalSongs);
-    };
-
-    const handleFileToggleNat = () => {
-        setSongs(nationalSongs);
-    };
-
-    const fetchData = async (dataSource: string): Promise<Song[]> => {
-        try {
-            const response = await fetch(`data/${dataSource}.csv`);
-            const reader = response.body?.getReader();
-            const result = await reader?.read();
-            const decoder = new TextDecoder('utf-8');
-            const csv = decoder.decode(result?.value);
-            const parsedData = Papa.parse<Song>(csv, { header: true, skipEmptyLines: true });
-            return parsedData.data;
-        } catch (error) {
-            console.error(`Error fetching ${dataSource} data:`, error);
-            return [];
-        }
-    };
-
-    useEffect(() => {
-        const loadSongs = async () => {
-            const [national, international] = await Promise.all([
-                fetchData('Nacional'),
-                fetchData('Internacional')
-            ]);
-            setNationalSongs(national);
-            setInternationalSongs(international);
-            setSongs(international);
-        };
-
-        loadSongs();
-    }, []);
-
-    useEffect(() => {
-        const filtered = songs.filter(song =>
-            song.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-            song.author.toLowerCase().includes(searchQuery.toLowerCase())
-        );
-        setCurrentPage(1);
-        setFilteredSongs(filtered);
-    }, [songs, searchQuery]);
-
-    const indexOfLastSong = currentPage * songsPerPage;
-    const indexOfFirstSong = indexOfLastSong - songsPerPage;
-
-    const currentSongs = filteredSongs.slice(indexOfFirstSong, indexOfLastSong);
-
-    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
-
-    return (
-        <>
-            <button onClick={handleFileToggleInt}>Internacional</button>
-            <button onClick={handleFileToggleNat}>Nacional</button>
-            <SearchBar setSearchQuery={setSearchQuery} />
-            <Pagination
-                songsPerPage={songsPerPage}
-                totalSongs={filteredSongs.length}
-                paginate={paginate}
-            />
-            <SongList songs={currentSongs} />
-        </>
-    );
-};
-
-export default App;
- */
-
 import React, { useState, useEffect } from 'react';
 import Papa from 'papaparse';
 import Pagination from './components/Pagination';
 import SearchBar from './components/SearchBar';
 import SongList from './components/SongList';
 import { StyledContainer, StyledButtonGroup, StyledButton } from './styles';
+import QRCode from "react-qr-code";
 
 interface Song {
     name: string;
@@ -180,6 +88,14 @@ const App: React.FC = () => {
                 totalSongs={filteredSongs.length}
                 paginate={paginate}
             />
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
+                <QRCode
+                    size={10}
+                    style={{ height: "auto", maxWidth: "50%", width: "50%" }}
+                    value={"https://henrichris.github.io/karaoke/"}
+                    viewBox={`0 0 256 256`}
+                />
+            </div>
         </StyledContainer>
     );
 };
